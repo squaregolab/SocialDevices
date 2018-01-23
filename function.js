f875698d4b18fadd857bc646dc47a702bf20ae87
Christen-Lou <35691981+Christen-Lou@users.noreply.github.com>
2018-01-23 15:40:49 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,11 +1,11 @@
-// SOCIALDEVICES V2.4 2018
-function open_option(){
-     window.open('page_option.php','parametre','menubar=no, scrollbars=no, top=100, left=100, width=400, height=200');
+// SOCIALDEVICES V2.5 2018
+function open_option_filter(){
+     window.open('page_filter.php','parametre','menubar=no, scrollbars=no, top=100, left=100, width=400, height=200');
 }
-function open_design(){
-     window.open('page_design.php','parametre','menubar=no, scrollbars=no, top=100, left=100, width=400, height=200');
+function open_option_adress(){
+     window.open('page_adress.php','parametre','menubar=no, scrollbars=no, top=100, left=100, width=400, height=200');
 }
-function Restart_counter(){
+function open_option_counter(){
 	 window.open('page_counter.php','parametre','menubar=no, scrollbars=no, top=100, left=100, width=500, height=150');
 }
 function CloseWindows(){
