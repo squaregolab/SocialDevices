5a997fef3292205f3e4c617459b8366fbf484e1a
Christen-Lou <35691981+Christen-Lou@users.noreply.github.com>
2018-01-23 21:04:22 +0100

    Update page_counter.php

--- page_counter.php
+++ page_counter.php
@@ -1,4 +1,5 @@
 <!-- SOCIALDEVICES V2.5 2018 - PopUp ReturnZero TweetNumber Page -->
+<!-- Author : clbouchier -->
 <!doctype html>
 <html><meta charset="utf-8"/><!-- for specials caracters -->
 	<head>
