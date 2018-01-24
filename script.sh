fcf6ec24de0ccc2267be56970bdb49362c036fa1
Thibaud LOREK <35608666+ThibaudLOREK@users.noreply.github.com>
2018-01-24 18:37:05 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#Attention ce code fonction de facon brutale, il ne peut etre arrete que par la fermutre du terminal 
 cd /var/www/html
 python Recup_20.py
+while [ 1 ]
+do
 python SocialDevice.py
+done
