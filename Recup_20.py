14f8fe47cc838add5595078852dc9e68d4b599dd
Thibaud LOREK <35608666+ThibaudLOREK@users.noreply.github.com>
2018-01-24 18:34:57 +0100

    Update Recup_20.py

--- Recup_20.py
+++ Recup_20.py
@@ -17,14 +17,12 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-#incremente le compteur de tweets sur l'afficheur à segments
 def increment_tweet(nb_new_tweet):
 	try:
 		with open("Counter.txt", "r") as fichier:
 			contenu = fichier.read()
     		fichier.close()
 		nombre = int(float(contenu[:-1]))+nb_new_tweet
-		#vérifiaction que le nombre ne dépasse pas 9999(nombre maximum possible sur l'afficheiur à segments
 		if nombre > 9999:
 			nombre = 0
 		with open("Counter.txt", "w") as fichier:
@@ -37,26 +35,22 @@ def increment_tweet(nb_new_tweet):
 		fichier.close()
 		ser.write(str(nb_new_tweet)+"t")
 	time.sleep(2)
-	
-#La fonction va lire le nombre de like sur la page choisit par l'utilisateur
+
 def lire_like():
-    with open("facebook.txt", "r") as fichier:
+	with open("Facebook.txt", "r") as fichier:
 		contenu = fichier.read()
-    	fichier.close()
-	
-	html = urlopen('https://www.facebook.com/'contenu+'/').read()
+	fichier.close()
+	html = urlopen('https://www.facebook.com/'+contenu+'/').read()
 	soup = BeautifulSoup.BeautifulSoup(html, "html.parser")
 
 	letters = soup.findAll('div', {"class":"_2pi9 _2pi2"})[0].div.text
+	i=0
+	nb_like=""
+	while letters[i]!=' ':
+		nb_like=nb_like+letters[i]
+		i=i+1
+	ser.write('f'+str(nb_like))
 
-    i=0
-    nb_like=""
-    while letters[i]!=' ':
-        nb_like=nb_like+letters[i]
-        i=i+1
-    ser.write('f'+str(nb_like))
-
-#Fonction convertissant les .json générer ecrire_json() par  en .xml
 def ecrire_xml():
     with open("fichier.json", "r") as fichier:
         old = json.load(fichier)
@@ -68,7 +62,6 @@ def ecrire_xml():
     mon_fichier.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?>\n")
     mon_fichier.write("<donnees>\n")
 
-    #Range les tweets du plus récent au plus ancien
     while i <= len(old)-1:
         mon_fichier.write("<post id=\"post-")
         text = str(y) +"\" value=\""
@@ -97,14 +90,12 @@ def ecrire_xml():
     fichier.close()
 
 
-#Lis le filtre choisit par l'utilisateur
 def chargement_hashtag():
     with open("Filter.txt", "r") as fichier:
         TERMS = fichier.read()
     fichier.close()
     return TERMS
 
-#Créer une speudo base de données en .json
 def ecrire_json(user_name_recu, url_avatar_recu, text_recu, media_recu,created_at_recu, platform_recu):
     class Post:
 
@@ -149,20 +140,16 @@ def ecrire_json(user_name_recu, url_avatar_recu, text_recu, media_recu,created_a
     fichier.close()
     ecrire_xml()
 
-#Remplacez **** par vos clé
 APP_KEY = '****'
 APP_SECRET = '****'
 OAUTH_TOKEN = '****'
 OAUTH_TOKEN_SECRET = '****'
 
-#Définition de l'addresse de l'arduino
 ser = serial.Serial('/dev/ttyACM0', 9600)
-#attente afin de ne laisser le temps à l'Arduino d'être fonctionnelle avec son port série 
 time.sleep(2)
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-#Configuration pour envoyer une requête demandant les 20 derniers tweets avec le filtre choisit
 search_results = twitter.search(count=20, q=chargement_hashtag())
 
 try:
@@ -173,7 +160,6 @@ try:
         created_at_recu = tweet['created_at']
         created_at_recu=created_at_recu[:20]+created_at_recu[26]+created_at_recu[27]+created_at_recu[28]+created_at_recu[29]
         platform_recu = 'twitter'
-	#vérification de la présence une image ou une video joint au tweet
         if 'media' in tweet['entities'].keys():
             media = tweet['entities']['media']
             for mediaitem in media:
@@ -186,7 +172,6 @@ try:
 except TwythonError as e:
     print(e)
 
-#incrémente le compteur de tweets directement de 20
 increment_tweet(20)
 
 try:
