31eba56b877256b52dcc1539ced3e9a28c3e55ad
Thibaud LOREK <35608666+ThibaudLOREK@users.noreply.github.com>
2018-01-24 18:33:35 +0100

    Update SocialDevice.py

--- SocialDevice.py
+++ SocialDevice.py
@@ -17,7 +17,6 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-#incremente le compteur de tweets sur l'afficheur à segments
 def increment_tweet():
 	try:
 		with open("Counter.txt", "r") as fichier:
@@ -43,7 +42,7 @@ def lire_like():
 		contenu = fichier.read()
     	fichier.close()
 	
-	html = urlopen('https://www.facebook.com/'contenu+'/').read()
+	html = urlopen('https://www.facebook.com/'+contenu+'/').read()
 	soup = BeautifulSoup.BeautifulSoup(html, "html.parser")
 
 	letters = soup.findAll('div', {"class":"_2pi9 _2pi2"})[0].div.text
@@ -56,7 +55,7 @@ def lire_like():
 	ser.write('f'+str(nb_like))
 	print(str(nb_like))
 
-#Fonction convertissant les .json générer ecrire_json() par  en .xml
+#Fonction convertissant les .json generer ecrire_json() par  en .xml
 def ecrire_xml():
 	with open("fichier.json", "r") as fichier:
 		old = json.load(fichier)
@@ -94,14 +93,13 @@ def ecrire_xml():
 	mon_fichier.write("</donnees>")
 	fichier.close()
 
-#Lis le filtre choisit par l'utilisateur
 def chargement_hashtag():
 	with open("Filter.txt", "r") as fichier:
 		TERMS = fichier.read()
 	fichier.close()
 	return TERMS
 
-#Créer une speudo base de données en .json
+#Creer une speudo base de donnees en .json
 def ecrire_json(user_name_recu, url_avatar_recu, text_recu, media_recu,created_at_recu, platform_recu):
 	class Post:
 
@@ -131,7 +129,7 @@ def ecrire_json(user_name_recu, url_avatar_recu, text_recu, media_recu,created_a
 			liste_posts.extend(old)
 			if len(liste_posts) > 19:
 				del liste_posts[0]
-	#Si le .json n'existe, un nouveau est créé
+	#Si le .json n'existe, un nouveau est cree
 	except:
 		with open("fichier.json", 'w') as fichier:
 			json.dump(new, fichier, indent=4)
@@ -146,16 +144,15 @@ def ecrire_json(user_name_recu, url_avatar_recu, text_recu, media_recu,created_a
 	fichier.close()
 	ecrire_xml()
 
-#Remplacez **** par vos clé
+#Remplacez **** par vos clefs
 APP_KEY = '****'
 APP_SECRET = '****'
 OAUTH_TOKEN = '****'
 OAUTH_TOKEN_SECRET = '****'
 
-#Définition de l'addresse de l'arduino
 ser = serial.Serial('/dev/ttyACM0', 9600)
-#attente afin de ne laisser le temps à l'Arduino d'être fonctionnelle avec son port série 
 time.sleep(2)
+
 
 class MyStreamer(TwythonStreamer):
 	def on_success(self, data):
@@ -167,7 +164,7 @@ class MyStreamer(TwythonStreamer):
 			created_at_recu = data['created_at']
 			created_at_recu=created_at_recu[:20]+created_at_recu[26]+created_at_recu[27]+created_at_recu[28]+created_at_recu[29]
 			platform_recu = 'twitter'
-			#vérification de la présence une image ou une video joint au tweet
+			#verification de la presence une image ou une video joint au tweet
 			if 'media' in data['entities'].keys():
 				media = data['entities']['media']
 				for mediaitem in media:
